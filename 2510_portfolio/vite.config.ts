c13351c751b35b4f8e63e73c1995baed88cdaa40
박수휘 <tngnl12341@gmail.com>
2025-10-25 17:03:05 +0900

    config) 파일 경로 지정 설정 추가

--- 2510_portfolio/vite.config.ts
+++ 2510_portfolio/vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +11,13 @@ export default defineConfig({
       },
     }),
   ],
+  // 파일 경로 지정
+  resolve: {
+    alias: {
+      styles: path.resolve(__dirname, 'src/styles'),
+      data: path.resolve(__dirname, 'src/data'),
+      layout: path.resolve(__dirname, 'src/components/layout'),
+      sections: path.resolve(__dirname, 'src/components/sections'),
+    },
+  },
 })
