440cf4f16f8bee56c47570a96fa279b28211c2a0
박수휘 <tngnl12341@gmail.com>
2025-10-25 17:09:47 +0900

    layout) footer 파일 생성

---
+++ 2510_portfolio/src/components/layout/footer/Footer.tsx
@@ -0,0 +1,18 @@
+/**
+ * components/layout
+ * Footer.tsx
+**/
+
+import styles from './footer.module.scss';
+
+function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className="hwiInner">
+        푸터
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
