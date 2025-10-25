96f6c0d98ccffefedba133a389d7ecbfa7dbe0e5
박수휘 <tngnl12341@gmail.com>
2025-10-25 17:10:11 +0900

    components > sections) project 관련 파일 생성

---
+++ 2510_portfolio/src/components/sections/projects/ProjectList.tsx
@@ -0,0 +1,28 @@
+/**
+ * components/sections
+ * projects/ProjectList.tsx
+**/
+
+import styles from './projects.module.scss';
+
+// import { projects } from '@/data/projects';
+// import ProjectCard from './ProjectCard';
+
+function ProjectList() {
+  return (
+    <section id="projects">
+      <h3 className="hwiTitle">Projects</h3>
+      project list
+    </section>
+//     <ul className={styles.projectList}>
+// asdf
+//     </ul>
+    //  <ul className={styles.list}>
+    //   {projects.map((project) => (
+    //     <ProjectCard key={project.id} project={project} />
+    //   ))}
+    // </ul>
+  )
+}
+
+export default ProjectList;
---
+++ 2510_portfolio/src/components/sections/projects/ProjectModal.tsx
@@ -0,0 +1,4 @@
+/**
+ * components/sections
+ * projects/ProjectModal.tsx
+**/
