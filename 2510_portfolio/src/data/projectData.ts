f32f0e852a8d5a70aca97a2fd1a1bdda2f929573
박수휘 <tngnl12341@gmail.com>
2025-11-11 10:32:29 +0900

    전체) 이미지 경로 변경

--- 2510_portfolio/src/data/projectData.ts
+++ 2510_portfolio/src/data/projectData.ts
@@ -3,7 +3,6 @@
  * projectData.ts
 **/
 
-
 export interface ProjectItem {
   id: string;
   imgSrc: string;
@@ -17,31 +16,34 @@ export interface ProjectItem {
 export const ProjectData: ProjectItem[] = [
   {
     id: 'portfolio',
-    imgSrc: '/src/assets/images/project/work_thumb-',
+    imgSrc: '/assets/images/project/work_thumb-',
     comp: '개인',
     title: '포트폴리오',
+    period: '약 3주',
     position: '1인 (기여도 100%)',
-    skill: 'React.js, SCSS, HTML5'
+    skill: 'React.js, TypeScript, SCSS, HTML5'
   },
   {
     id: 'hwitter',
-    imgSrc: '/src/assets/images/project/work_thumb-',
+    imgSrc: '/assets/images/project/work_thumb-hwitter.png',
     comp: '개인',
     title: 'Hwitter (SNS 클론)',
+    period: '약 2주',
     position: '1인 (기여도 100%)',
-    skill: 'React.js, SCSS, HTML5'
+    skill: 'React.js, TypeScript, SCSS, Firebase'
   },
   {
     id: 'movieApp',
-    imgSrc: '/src/assets/images/project/work_thumb-',
+    imgSrc: '/assets/images/project/work_thumb-movie.png',
     comp: '개인',
     title: '영화 앱 클론',
+    period: '약 2주',
     position: '1인 (기여도 100%)',
-    skill: 'React.js, styled-components, SCSS, HTML5'
+    skill: 'React (Vite 기반), TypeScript, Styled-components, Github-pages'
   },
   {
     id: 'hcgHr',
-    imgSrc: '/src/assets/images/project/work_thumb-hcg.png',
+    imgSrc: '/assets/images/project/work_thumb-hcg.png',
     comp: '(주)휴먼컨설팅그룹',
     title: '휴먼컨설팅그룹 HR 서비스 고도화',
     period: '2024.10 - 진행 중',
@@ -49,7 +51,7 @@ export const ProjectData: ProjectItem[] = [
   },
   {
     id: 'skBio',
-    imgSrc: '/src/assets/images/project/work_thumb-sk.png',
+    imgSrc: '/assets/images/project/work_thumb-sk.png',
     comp: '(주)휴먼컨설팅그룹',
     title: '"SK바이오사이언스" HR 서비스',
     period: '2024.07 - 2024.09 (2개월)',
@@ -58,7 +60,7 @@ export const ProjectData: ProjectItem[] = [
   },
   {
     id: 'gs',
-    imgSrc: '/src/assets/images/project/work_thumb-gs.png',
+    imgSrc: '/assets/images/project/work_thumb-gs.png',
     comp: '(주)휴먼컨설팅그룹',
     title: '"GS칼텍스" 통합 HR 플랫폼 개발',
     period: '2024.02 - 2024.08 (6개월)',
@@ -67,7 +69,7 @@ export const ProjectData: ProjectItem[] = [
   },
   {
     id: 'mmon',
-    imgSrc: '/src/assets/images/project/work_thumb-mmon.png',
+    imgSrc: '/assets/images/project/work_thumb-mmon.png',
     comp: '(주)엠몬스타',
     title: '엠몬 솔루션 신규 구축 (회사 자체 솔루션)',
     period: '2022.02 - 2023.01 (11개월)',
@@ -76,7 +78,7 @@ export const ProjectData: ProjectItem[] = [
   },
   {
     id: 'nStation',
-    imgSrc: '/src/assets/images/project/work_thumb-nstation.png',
+    imgSrc: '/assets/images/project/work_thumb-nstation.png',
     comp: '(주)엠몬스타',
     title: '"내셔널지오그래픽" 온라인몰 리뉴얼',
     period: '2021.08 - 2021.12 (5개월)',
@@ -85,7 +87,7 @@ export const ProjectData: ProjectItem[] = [
   },
   {
     id: 'hago',
-    imgSrc: '/src/assets/images/project/work_thumb-hago.png',
+    imgSrc: '/assets/images/project/work_thumb-hago.png',
     comp: '(주)엠몬스타',
     title: '"HAGO" 쇼핑몰 리뉴얼',
     period: '2021.05 - 2021.08 (4개월)',
@@ -94,7 +96,7 @@ export const ProjectData: ProjectItem[] = [
   },
   {
     id: 'fp',
-    imgSrc: '/src/assets/images/project/work_thumb-fp.png',
+    imgSrc: '/assets/images/project/work_thumb-fp.png',
     comp: '(주)엠몬스타',
     title: '"패션플러스" 2021 리뉴얼',
     period: '2020.09 - 2021.01 (5개월)',
